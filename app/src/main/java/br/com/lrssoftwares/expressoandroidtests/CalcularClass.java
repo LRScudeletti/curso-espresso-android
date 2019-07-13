55de0667ee0cc49205443d1b6ee715ec22cfc903
Luiz Rogério Scudeletti <rogerio.scudeletti@gmail.com>
2019-07-13 11:43:06 -0300

    Adicionado exemplo de teste de unidade

--- app/src/main/java/br/com/lrssoftwares/expressoandroidtests/CalcularClass.java
+++ app/src/main/java/br/com/lrssoftwares/expressoandroidtests/CalcularClass.java
@@ -1,26 +1,7 @@
 package br.com.lrssoftwares.expressoandroidtests;
 
-public class CalcularClass {
-    private double valor1;
-    private double valor2;
-
-    public double getValor1() {
-        return valor1;
-    }
-
-    public void setValor1(double valor1) {
-        this.valor1 = valor1;
-    }
-
-    public double getValor2() {
-        return valor2;
-    }
-
-    public void setValor2(double valor2) {
-        this.valor2 = valor2;
-    }
-
-    private double calcular(double valor1, double valor2) {
+class CalcularClass {
+    double calcular(double valor1, double valor2) {
         return valor1 + valor2;
     }
 }
--- app/src/test/java/br/com/lrssoftwares/expressoandroidtests/CalcularTest.java
+++ app/src/test/java/br/com/lrssoftwares/expressoandroidtests/CalcularTest.java
@@ -1,4 +1,19 @@
 package br.com.lrssoftwares.expressoandroidtests;
 
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
 public class CalcularTest {
+
+    @Test
+    public void teste_01_testarSoma() {
+        Random gerador = new Random();
+        double valor1 = gerador.nextDouble();
+        double valor2 = gerador.nextDouble();
+
+        assertEquals(new CalcularClass().calcular(valor1, valor2), valor1 + valor2, 0);
+    }
 }
