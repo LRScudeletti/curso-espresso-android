b6bd9be9e8cb32619bc67ce6c51bc1638296dd62
Luiz Rogério Scudeletti <rogerio.scudeletti@gmail.com>
2019-07-15 08:52:40 -0300

    Adicionado o Orchestrator

--- app/src/main/java/br/com/lrssoftwares/expressoandroidtests/LoginActivity.java
+++ app/src/main/java/br/com/lrssoftwares/expressoandroidtests/LoginActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class  LoginActivity extends AppCompatActivity {
 
     public String usuarioInformado = "Teste";
     public String senhaInformada = "123456";
